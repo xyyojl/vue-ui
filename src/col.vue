105e090d631307b8aab6157e92cdc1853af6b292
xyyojl <m13631897256@163.com>
2019-07-31 11:37:19 +0800

    添加 row 的 align=left/right/center 属性

--- src/col.vue
+++ src/col.vue
@@ -42,7 +42,7 @@ export default {
 </script>
 <style lang="scss" scoped>
     .col{
-        width: 50%;
+        // width: 50%;
         $class-prefix: col-;
         @for $n from 1 through 24{
             &.#{$class-prefix}#{$n}{
