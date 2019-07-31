704788220a1c48e183948a7e34ae074a99931048
xyyojl <m13631897256@163.com>
2019-07-31 10:55:32 +0800

    初步实现 row 和 col

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'GiluCol',
+    props:{
+        span:{
+            type: [Number, String]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .col{
+        height: 100px;
+        background-color: grey;
+        width: 50%;
+        border: 1px solid red;
+        $class-prefix: col-;
+        @for $n from 1 through 24{
+            &.#{$class-prefix}#{$n}{
+                width: ($n / 24) * 100%;
+            }
+        }
+    }
+</style>
