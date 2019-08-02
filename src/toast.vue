f125e74b91947140303cd1fafc86784795e1971b
xyyojl <m13631897256@163.com>
2019-08-02 15:20:19 +0800

    实现 position: top/bottom/middle 功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast" ref="wrapper">
+  <div class="toast" ref="wrapper" :class="toastClasses">
     <div class="message">
       <slot v-if="!enableHtml"></slot>
       <div v-else v-html="$slots.default[0]"></div>
@@ -34,11 +34,25 @@ export default {
     enableHtml: {
       type: Boolean,
       default: false
+    },
+    position: {
+      type: String,
+      default: "top",
+      validator(value) {
+        return ["top", "middle", "bottom"].indexOf(value) >= 0;
+      }
     }
   },
   mounted() {
     this.updateStyles();
     this.execAutoClose();
+  },
+  computed: {
+    toastClasses() {
+      return {
+        [`position-${this.position}`]: true
+      };
+    }
   },
   methods: {
     updateStyles() {
@@ -77,9 +91,6 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   line-height: 1.8;
   min-height: $toast-min-height;
   position: fixed;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
   display: flex;
   align-items: center;
   background: $toast-bg;
@@ -87,6 +98,7 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
   color: #fff;
   padding: 0 16px;
+  left: 50%;
   .message {
     padding: 8px 0;
   }
@@ -98,6 +110,18 @@ $toast-bg: rgba(0, 0, 0, 0.75);
     height: 100%;
     border-left: 1px solid #666;
     margin-left: 16px;
+  }
+  &.position-top {
+    top: 0;
+    transform: translateX(-50%);
+  }
+  &.position-bottom {
+    bottom: 0;
+    transform: translateX(-50%);
+  }
+  &.position-middle {
+    top: 50%;
+    transform: translate(-50%,-50%);
   }
 }
 </style>
