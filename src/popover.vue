37257ba53cb768cc1f04a71d3b457d58934bfd5f
xyyojl <m13631897256@163.com>
2019-08-03 22:01:47 +0800

    基本实现功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+    <div class="popover" @click="onClick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
@@ -19,7 +19,7 @@ export default {
         }
     },
     methods:{
-        xxx(){
+        /* xxx(){
             this.visible = !this.visible
             if(this.visible === true){
                 this.$nextTick(()=>{
@@ -36,6 +36,38 @@ export default {
                 })
             }else{
                 console.log('vm 隐藏 popover')
+            }
+        } */
+        positionContent(){
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+        },
+        onClickDocument(e){
+            if (this.$refs.popover && 
+                (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+            ) { return;}
+            this.close()
+        },
+        open(){
+            this.visible = true
+            this.$nextTick(()=>{
+                this.positionContent()
+                document.addEventListener('click', this.onClickDocument)
+            })
+        },
+        close(){
+            this.visible = false
+            document.removeEventListener('click', this.onClickDocument)
+        },
+        onClick(event){
+            if (this.$refs.triggerWrapper.contains(event.target)) {
+                if(this.visible === true){
+                    this.close()
+                }else{
+                    this.open()
+                }
             }
         }
     }
