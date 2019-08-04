d3cbd8674e0959f04e4cbb3f6da294df79f159cb
xyyojl <m13631897256@163.com>
2019-08-04 16:59:25 +0800

    可以设置默认 selected

--- src/collapse.vue
+++ src/collapse.vue
@@ -11,6 +11,9 @@ export default {
         single:{
             type: Boolean,
             default: false
+        },
+        selected:{
+            type: String
         }
     },
     data(){
@@ -19,11 +22,15 @@ export default {
         }
     },
     provide(){
-        if(this.single){
-            return {
-                eventBus: this.eventBus
-            }
+        return {
+            eventBus: this.eventBus
         }
+    },
+    mounted(){
+        this.eventBus.$emit('update:selected',this.selected)
+        this.eventBus.$on('update:selected',(name)=>{
+            this.$emit('update:selected',name)
+        })
     }
 }
 </script>
