c4c06b3920afd151a6abb644e43c570b6b5a4583
xyyojl <m13631897256@163.com>
2019-08-04 17:26:27 +0800

    将selected 改为数组

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -30,26 +30,20 @@ export default {
     inject: ['eventBus'],
     mounted(){
         this.eventBus && this.eventBus.$on('update:selected',(name)=>{
-            if(name !== this.name){
-                this.close()
+            if(name.indexOf(this.name) >= 0){
+                this.open = true
             }else{
-                this.show()
+                this.open = false
             }
         })
     },
     methods:{
         toggle(){
             if(this.open){
-                this.open = false
+                this.eventBus && this.eventBus.$emit('update:removeSelected',this.name)
             }else{
-                this.eventBus && this.eventBus.$emit('update:selected',this.name)
+                this.eventBus && this.eventBus.$emit('update:addSelected',this.name)
             }
-        },
-        close(){
-            this.open = false
-        },
-        show(){
-            this.open = true
         }
     }
 }
