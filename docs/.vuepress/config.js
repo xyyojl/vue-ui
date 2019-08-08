d578f8ae0ef72632b3965fe8a2fcc4ad06d118cd
xyyojl <m13631897256@163.com>
2019-08-08 11:44:32 +0800

    创建官网

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: 'gView UI',
+    description: '一个好用的UI框架',
+    themeConfig: {
+        sidebar: [
+            {
+                title: '入门',
+                children: [
+                    '/install/',
+                    '/get-started/',
+                ]
+            },
+            {
+                title: '组件',
+                children: [
+                    '/components/button',
+                    '/components/input',
+                    '/components/grid',
+                    '/components/layout',
+                    '/components/toast',
+                    '/components/tabs',
+                    '/components/popover',
+                    '/components/collpase',
+                ]
+            },
+        ],
+    }
+}
---
+++ docs/components/button.md
@@ -0,0 +1,14 @@
+---
+title: Button
+---
+# 按钮
+
+使用方法
+
+<button-demos/>
+```html
+<g-button>默认按钮</g-button>
+<g-button icon="settings">默认按钮</g-button>
+<g-button :loading="true">默认按钮</g-button>
+<g-button disabled>默认按钮</g-button>
+```
