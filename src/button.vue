c49ef7a1ec95571ca2ce9fd8865c13b9871c2a56
xyyojl <m13631897256@163.com>
2019-08-08 12:16:58 +0800

    修复一个 bug

--- src/button.vue
+++ src/button.vue
@@ -1,65 +1,101 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"
-    @click="$emit('click')">
-    <g-icon class="icon" v-if="icon && !loading" :name="icon"/>
-    <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
-    <div class="g-button-content">
-      <slot/>
-    </div>
-  </button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+        @click="$emit('click')">
+        <!-- <svg class="icon" v-if="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg> -->
+        <g-icon v-if="icon && !loading" :name="icon" class="icon"></g-icon>
+        <g-icon name="loading" v-if="loading" class="loading icon"></g-icon>
+        <div class="content">
+            <slot></slot>   
+        </div>
+    </button>
 </template>
 <script>
-  import Icon from './icon'
-  export default {
-    name: 'GuluButton',
-    components: {
-      'g-icon': Icon
-    },
-    props: {
-      icon: {},
-      loading: {
-        type: Boolean,
-        default: false
-      },
-      iconPosition: {
-        type: String,
-        default: 'left',
-        validator (value) {
-          return value === 'left' || value === 'right'
+    import Icon from './icon';
+    export default {
+        name:'UiButton',
+        components:{
+            'g-icon':Icon
+        },
+        // props:['icon','iconPosition'] // left right
+        props:{
+            icon:{},
+            loading:{
+                type:Boolean,
+                default: false
+            },
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator(value){
+                    return !(value !== 'left' && value !== 'right');
+                    // return value === 'left' || value === 'right'
+
+                    /* if(value !== 'left' && value !== 'right'){
+                        return false
+                    }else{
+                        return true
+                    } */
+                }
+            }
         }
-      }
     }
-  }
 </script>
 <style lang="scss" scoped>
-  $font-size: 14px;
-  $button-height: 32px;
-  $button-bg: white;
-  $button-active-bg: #eee;
-  $border-radius: 4px;
-  $color: #333;
-  $border-color: #999;
-  $border-color-hover: #666;
-  @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }
-  .g-button { font-size: $font-size; height: $button-height; padding: 0 1em;
-    border-radius: $border-radius; border: 1px solid $border-color;
-    background: $button-bg;
-    display: inline-flex; justify-content: center; align-items: center;
-    vertical-align: middle;
-    &:hover { border-color: $border-color-hover; }
-    &:active { background-color: $button-active-bg; }
-    &:focus { outline: none; }
-    > .g-button-content { order: 2; }
-    > .icon { order: 1; margin-right: .1em; }
-    &.icon-right {
-      > .g-button-content { order: 1; }
-      > .icon { order: 2; margin-right: 0; margin-left: .1em;}
+    $button-height: 32px;
+    $font-size: 14px;
+    $button-bg: white;
+    $button-active-bg: #eee;
+    $border-radius: 4px;
+    $color: #333;
+    $border-color: #999;
+    $border-color-hover: #666;
+    @keyframes spin {
+        0%{transform: rotate(0deg);}
+        100%{transform: rotate(360deg);}
     }
-    .loading {
-      animation: spin 2s infinite linear;
+    .g-button{
+        height: $button-height;
+        font-size: $font-size;
+        padding: 0 1em;
+        border-radius: $border-radius;
+        border: 1px solid $border-color;
+        background: $button-bg;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover{
+            border-color: $border-color-hover;
+        }
+        &:active{
+            background-color: $button-active-bg;
+        }
+        &:focus{
+            outline: none;
+        }
+        > .content{
+            order: 2;
+        }
+        > .icon{
+            order: 1;
+            margin-right: .1em;
+        }
+        
+        &.icon-right{
+            > .content{
+                order: 1;
+            }
+            > .icon{
+                order: 2;
+                margin-right: 0;
+                margin-left: .1em;
+            }
+        }
+        .loading{
+            animation: spin 2s infinite linear;
+        }
     }
-  }
+
 </style>
